5036a991daa8ad9396eef2a4547b89843cdcd9e4
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2019-11-24 16:04:10 -0500

    Export polyfills and setupURLPolyfill

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import {polyfillGlobal} from 'react-native/Libraries/Utilities/PolyfillFunctions
 
 import {name, version} from './package.json';
 
-global.REACT_NATIVE_URL_POLYFILL = `${name}@${version}`;
+export * from './js/URL';
+export * from './js/URLSearchParams';
 
-polyfillGlobal('URL', () => require('./js/URL').URL);
-polyfillGlobal(
-  'URLSearchParams',
-  () => require('./js/URLSearchParams').URLSearchParams,
-);
-polyfillGlobal('Buffer', () => require('buffer').Buffer);
+export function setupURLPolyfill() {
+  global.REACT_NATIVE_URL_POLYFILL = `${name}@${version}`;
+
+  polyfillGlobal('URL', () => require('./js/URL').URL);
+  polyfillGlobal(
+    'URLSearchParams',
+    () => require('./js/URLSearchParams').URLSearchParams,
+  );
+  polyfillGlobal('Buffer', () => require('buffer').Buffer);
+}
+
+setupURLPolyfill();
