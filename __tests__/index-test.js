59a5180c7b36cd713bca66b4baf087361a330d87
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2019-11-24 19:35:04 -0500

    Add tests for auto and index

---
+++ __tests__/index-test.js
@@ -0,0 +1,44 @@
+import * as PolyfillFunctions from 'react-native/Libraries/Utilities/PolyfillFunctions';
+
+jest.mock('react-native/Libraries/Utilities/PolyfillFunctions', () => ({
+  polyfillGlobal: jest.fn(),
+}));
+
+describe('Index', function() {
+  it('should apply Buffer polyfill', () => {
+    require('../index');
+
+    expect(PolyfillFunctions.polyfillGlobal).toBeCalledTimes(1);
+    expect(PolyfillFunctions.polyfillGlobal).toBeCalledWith(
+      'Buffer',
+      expect.any(Function),
+    );
+  });
+
+  it("shouldn't apply URL and URLSearchParams on import", () => {
+    expect(global.REACT_NATIVE_URL_POLYFILL).toBeUndefined();
+
+    require('../index');
+
+    expect(PolyfillFunctions.polyfillGlobal).toBeCalledTimes(1); // For Buffer
+    expect(global.REACT_NATIVE_URL_POLYFILL).toBeUndefined();
+  });
+
+  it('should export setupURLPolyfill', () => {
+    const imports = require('../index');
+
+    expect(imports.setupURLPolyfill).toBeDefined();
+  });
+
+  it('should export URL', () => {
+    const imports = require('../index');
+
+    expect(imports.URL).toBeDefined();
+  });
+
+  it('should export URLSearchParams', () => {
+    const imports = require('../index');
+
+    expect(imports.URLSearchParams).toBeDefined();
+  });
+});
