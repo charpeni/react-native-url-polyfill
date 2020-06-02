e361ec8c8d9cb87da0d5fee12f14055442b2deb3
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2020-06-02 17:06:53 -0400

    Setup react-native-url-polyfill with RN 0.60

--- detox/rn-0.60/metro.config.js
+++ detox/rn-0.60/metro.config.js
@@ -5,7 +5,23 @@
  * @format
  */
 
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+const reactNativeLib = path.resolve(__dirname, '../..');
+
 module.exports = {
+  watchFolders: [reactNativeLib],
+  resolver: {
+    blacklistRE: blacklist([
+      new RegExp(`${reactNativeLib}/node_modules/react-native/.*`),
+      new RegExp(`${reactNativeLib}/detox\/((?!rn-0\.60).).*`),
+      new RegExp(path.resolve(__dirname, 'ios/.*')),
+    ]),
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
