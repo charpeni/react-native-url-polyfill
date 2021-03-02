5985d7efc07b496b829883540d09c6f0be384387
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2021-03-02 11:53:57 -0500

    Fix createObjectURL (#267)
    * Add tests for Blob
    
    * Configure Blob on Android
    
    * Use BlobModule instead of NativeBlobModule
    
    Co-authored-by: André Costa Lima <andreclima.pt@gmail.com>
    Co-authored-by: Kenny Meyer <kenny@kennymeyer.net>

--- detox/rn-0.62/e2e/url-polyfill.spec.js
+++ detox/rn-0.62/e2e/url-polyfill.spec.js
@@ -24,4 +24,10 @@ describe('URL Polyfill', () => {
       'https://facebook.github.io/react-native/img/header_logo.png',
     );
   });
+
+  it('should handle test 3', async () => {
+    await expect(element(by.id('url-test-3'))).toHaveText(
+      'blob:1?offset=32&size=64',
+    );
+  });
 });
