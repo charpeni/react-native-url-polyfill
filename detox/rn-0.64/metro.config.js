cc25f2b714c0f4185146e7e49d26d27805b42014
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2021-04-14 11:09:13 -0400

    Bump React and React Native to the latest version (#281)

--- detox/rn-0.64/metro.config.js
+++ detox/rn-0.64/metro.config.js
@@ -6,18 +6,17 @@
  */
 
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
 
 const reactNativeLib = path.resolve(__dirname, '../..');
 
 module.exports = {
   watchFolders: [reactNativeLib],
   resolver: {
-    blacklistRE: blacklist([
+    blockList: [
       new RegExp(`${reactNativeLib}/node_modules/react-native/.*`),
       new RegExp(`${reactNativeLib}/detox\/((?!rn-0\.64).).*`),
       new RegExp(path.resolve(__dirname, 'ios/.*')),
-    ]),
+    ],
     extraNodeModules: {
       'react-native': path.resolve(__dirname, 'node_modules/react-native'),
     },
