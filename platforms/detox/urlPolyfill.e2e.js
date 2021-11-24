d5bf8bf0d8963d41f49f9974a65da83437927d2d
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2021-11-24 13:10:43 -0500

    Refactor detox directory into platforms (#370)

---
+++ platforms/detox/urlPolyfill.e2e.js
@@ -0,0 +1,35 @@
+describe('URL Polyfill', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have REACT_NATIVE_URL_POLYFILL', async () => {
+    const {name, version} = require('../../package.json');
+
+    await expect(element(by.id('url-polyfill-version'))).toHaveText(
+      `${name}@${version}`,
+    );
+  });
+
+  it('should handle test 1', async () => {
+    await expect(element(by.id('url-test-1'))).toHaveText(
+      'https://google.dev/dev',
+    );
+  });
+
+  it('should handle test 2', async () => {
+    await expect(element(by.id('url-test-2'))).toHaveText(
+      'https://facebook.github.io/react-native/img/header_logo.png',
+    );
+  });
+
+  it('should handle test 3', async () => {
+    await expect(element(by.id('url-test-3'))).toHaveText(
+      'blob:1?offset=32&size=64',
+    );
+  });
+});
---
+++ platforms/react-native/0.62/metro.config.js
@@ -0,0 +1,33 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+const reactNativeLib = path.resolve(__dirname, '../../..');
+
+module.exports = {
+  watchFolders: [reactNativeLib],
+  resolver: {
+    blacklistRE: blacklist([
+      new RegExp(`${reactNativeLib}/node_modules/react-native/.*`),
+      new RegExp(`${reactNativeLib}/platforms/react-native/((?!0.62).).*`),
+      new RegExp(path.resolve(__dirname, 'ios/.*')),
+    ]),
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
