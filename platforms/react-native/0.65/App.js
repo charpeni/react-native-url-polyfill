623ad9f357db1dbeb9d9d7db93e45e87cbede7ea
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2021-12-13 17:07:22 -0500

    Use <TestPolyfill> in 0.65 and 0.66

--- platforms/react-native/0.65/App.js
+++ platforms/react-native/0.65/App.js
@@ -9,33 +9,14 @@
 import React from 'react';
 import {SafeAreaView, Text, StatusBar} from 'react-native';
 
+import TestPolyfill from '../../detox/TestPolyfill';
+
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text testID="url-polyfill-version">
-          {global.REACT_NATIVE_URL_POLYFILL}
-        </Text>
-        <Text testID="url-test-1">
-          {new URL('dev', 'https://google.dev').href}
-        </Text>
-        <Text testID="url-test-2">
-          {
-            new URL(
-              'https://facebook.github.io/react-native/img/header_logo.png',
-            ).href
-          }
-        </Text>
-        <Text testID="url-test-3">
-          {URL.createObjectURL({
-            data: {
-              blobId: 1,
-              offset: 32,
-            },
-            size: 64,
-          })}
-        </Text>
+        <TestPolyfill />
       </SafeAreaView>
     </>
   );
