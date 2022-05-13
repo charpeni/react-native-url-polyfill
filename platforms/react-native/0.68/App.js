b10dcbbf73a9175f3470f885c605dae4b26bdcfb
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2022-05-13 16:12:44 -0400

    Add tests for React Native 0.68 (#438)

---
+++ platforms/react-native/0.68/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+import TestPolyfill from '../../detox/TestPolyfill';
+
+const App: () => Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <TestPolyfill />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ platforms/react-native/0.68/metro.config.js
@@ -0,0 +1,33 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+
+const reactNativeLib = path.resolve(__dirname, '../../..');
+
+module.exports = {
+  watchFolders: [reactNativeLib],
+  resolver: {
+    blockList: [
+      new RegExp(`${reactNativeLib}/node_modules/react-native/.*`),
+      new RegExp(`${reactNativeLib}/platforms/react-native/((?!0.68).).*`),
+      new RegExp(`${reactNativeLib}/platforms/expo/.*`),
+      new RegExp(path.resolve(__dirname, 'ios/.*')),
+    ],
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
