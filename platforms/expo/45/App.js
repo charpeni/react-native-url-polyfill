8bec6724945ade6a7f1d4cf735bb6680b393831b
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2022-05-14 09:18:19 -0400

    Configure Expo for react-native-url-polyfill

--- platforms/expo/45/App.js
+++ platforms/expo/45/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+
+import TestPolyfill from '../../detox/TestPolyfill';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TestPolyfill />
       <StatusBar style="auto" />
     </View>
   );
--- platforms/expo/45/index.js
+++ platforms/expo/45/index.js
@@ -1,4 +1,5 @@
-import { registerRootComponent } from 'expo';
+import 'react-native-url-polyfill/auto';
+import {registerRootComponent} from 'expo';
 
 import App from './App';
 
