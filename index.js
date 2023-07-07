86334a8838d36f84e0df2c29083da9226a0b82d1
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2023-07-07 13:45:49 -0400

    Only default export is available soon (#458)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import './js/ios10Fix';
 
-import {name, version} from './package.json';
+import packageJson from './package.json';
 
 export * from './js/URL';
 export * from './js/URLSearchParams';
 
 export function setupURLPolyfill() {
-  globalThis.REACT_NATIVE_URL_POLYFILL = `${name}@${version}`;
+  globalThis.REACT_NATIVE_URL_POLYFILL = `${packageJson.name}@${packageJson.version}`;
 
   globalThis.URL = require('./js/URL').URL;
   globalThis.URLSearchParams = require('./js/URLSearchParams').URLSearchParams;
