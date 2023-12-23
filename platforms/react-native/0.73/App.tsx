e31ffca0cb702068404d4988518f61f333e06f28
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2023-12-23 12:36:31 -0500

    Initial setup for React Native 0.73

---
+++ platforms/react-native/0.73/App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+
+import TestPolyfill from '../../detox/TestPolyfill';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <TestPolyfill />
+    </SafeAreaView>
+  );
+}
+
+export default App;
