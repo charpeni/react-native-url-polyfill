3328c49ca7c2ba7c71e01d507309fa22dc23437e
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2025-09-22 18:13:40 -0400

    Remove Expo tests

--- platforms/expo/48/jest-puppeteer.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
-module.exports = {
-  server: {
-    command: "yarn web",
-    port: 19006,
-    launchTimeout: 30000,
-  },
-};
