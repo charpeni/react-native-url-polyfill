b8c8fc965408a09480209364c95317ff1e6cf339
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2026-07-06 19:47:29 -0400

    Add React Native 0.84 test app

---
+++ platforms/react-native/0.84/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+
+import TestPolyfill from '../../detox/TestPolyfill';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <TestPolyfill />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ platforms/react-native/0.84/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'react-native',
+  moduleNameMapper: {
+    '^react$': '<rootDir>/node_modules/react',
+    '^react-native$': '<rootDir>/node_modules/react-native',
+  },
+};
